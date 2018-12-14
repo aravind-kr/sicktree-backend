41184597666715f3c79b1f8f0be5a2774e50abf3
Arvind <arvindsmvec@gmail.com>
2018-12-14 17:21:05 +0530

    prisma graphql configured

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+// const express = require("express");
+const next = require("next");
+const rp = require("request-promise");
 
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 const server = createServer();
 
 server.express.use(cookieParser());
 
 // /dashboard  -> access
+// app.prepare().then(() => {
+//   const server = express();
+
+//   server.get("/dashboard", (req, res) => {
+//     return app.render(req, res, "/dashboard", req.query);
+//   });
+
+//   server.get("*", (req, res) => {
+//     return handle(req, res);
+//   });
+
+//   server.listen(3000, err => {
+//     if (err) throw err;
+//     console.log("> Ready on http://localhost:3000");
+//   });
+// });
 
 // decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
@@ -29,18 +52,65 @@ server.express.use(async (req, res, next) => {
   if (!req.userId) return next();
   const user = await db.query.user(
     { where: { id: req.userId } },
-    '{ id, permissions, email, name }'
+    "{ id, name }"
   );
   req.user = user;
   next();
+});
+
+server.express.get("/dashboard", async (req, res) => {
+  try {
+    const options = {
+      method: "POST",
+      uri: "https://api.instagram.com/oauth/access_token",
+      formData: {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        grant_type: "authorization_code",
+        redirect_uri: process.env.REDIRECT_URL,
+        code: req.query.code
+      },
+      json: true
+    };
+
+    let result = await rp(options);
+    
+    const user = await db.mutation.createUser(
+      {
+        data: {
+          access_token: result.access_token,
+          username: result.user.username,
+          profile_picture: result.user.profile_picture,
+          full_name: result.user.full_name,
+          bio: result.user.bio,
+          is_business: result.user.is_business,
+          website: result.user.website
+        }
+      },
+      "{ id, username }"
+    );
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+
+    res.redirect("http://localhost:7777/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+
+  // app.render('/test')
 });
 
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
+      origin: process.env.FRONTEND_URL
+    }
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,28 @@
-const mutations = {};
+const jwt = require('jsonwebtoken');
+
+const mutations = {
+  async signup(parent, args, ctx, info) {
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          name: "KR",
+          accessToken: "234"
+        }
+      },
+      info
+    );
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+
+    console.log('>>>',user, token);
+
+    return user    
+  }
+};
 
 module.exports = mutations;
